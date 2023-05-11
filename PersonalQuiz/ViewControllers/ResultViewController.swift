61c87ffc45cbff25ab066ff6576ded3842298f78
Vladimir <xalif80@gmail.com>
2023-05-11 12:39:24 +0200

    Initial Commit

---
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ResultViewController.swift
+//  PersonalQuiz
+//
+//  Created by Alexey Efimov on 19.02.2023.
+//
+
+import UIKit
+
+final class ResultViewController: UIViewController {
+    
+    @IBOutlet var resultLabelWithSmile: UILabel!
+    @IBOutlet var descriptionResultLabel: UILabel!
+    
+    var answers: [Answer]!
+    
+    // 1. Избавиться от кнопки возврата назад на экране результатов
+    // 2. Передать массив с ответами на экран с результатами
+    // 3. Определить наиболее часто встречающийся тип животного
+    // 4. Отобразить результаты в соответствии с этим животным
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.hidesBackButton = true
+        finalResult()
+    }
+    
+    @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
+    }
+    
+    private func finalResult() {
+        var mostOftenAnimals: [Animal: Int] = [:]
+        let animals = answers.map { $0.animal }
+        for animal in animals {
+            mostOftenAnimals[animal, default: 0] += 1
+        }
+        let sortmostOftenAnimals = mostOftenAnimals
+            .sorted { $0.value > $1.value }
+        guard let mostAnimal = sortmostOftenAnimals.first?.key
+        else { return }
+        
+        updateUI(with: mostAnimal)
+    }
+    
+    private func updateUI(with animal: Animal) {
+        resultLabelWithSmile.text = "Вы - \(animal.rawValue)!"
+        descriptionResultLabel.text = animal.definition
+    }
+}
